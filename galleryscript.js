1fb2e4a5343c2f9631dfe88a2a532fe1bf00005d
CosmicPower5329 <024-ccai@sd43.bc.ca>
2025-03-08 11:19:44 -0800

    Fixed Bug yay

--- galleryscript.js
+++ galleryscript.js
@@ -1,7 +1,7 @@
 const imageGallery = [
-    "https://static.wikia.nocookie.net/fisch/images/d/df/7557_kg_nessie.png/revision/latest/scale-to-width-down/1000?cb=20241103044458",
-    "https://static.wikia.nocookie.net/fisch/images/2/20/Aurora_Ancient_Kraken_V1.png/revision/latest/scale-to-width-down/185?cb=20250220090124",
-    "https://static.wikia.nocookie.net/fisch/images/d/d1/Aurora_Northstar_Serpent.png/revision/latest?cb=20241215091636"
+    "images/image.webp",
+    "images/image2.webp",
+    "images/image3.webp"
 ]
 const imageElement = document.getElementById("GI");
 const prevButton = document.getElementById("PrevBut");
@@ -13,7 +13,7 @@ function updateImage() {
 nextButton.addEventListener("click", function(){
     currentImgIndex = (currentImgIndex +1) % imageGallery.length
     updateImage()
-};
+});
 prevButton.addEventListener("click", function(){
     currentImgIndex = (currentImgIndex -1) % imageGallery.length
     updateImage()
