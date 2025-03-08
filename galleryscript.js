918fb20a11963e29d7ea59e381899971399406cd
CosmicPower5329 <024-ccai@sd43.bc.ca>
2025-03-08 10:38:33 -0800

    I AM DRINKING A PUMPKIN

---
+++ galleryscript.js
@@ -0,0 +1,9 @@
+const imageGallery = [
+    "https://static.wikia.nocookie.net/fisch/images/d/df/7557_kg_nessie.png/revision/latest/scale-to-width-down/1000?cb=20241103044458",
+    "https://static.wikia.nocookie.net/fisch/images/2/20/Aurora_Ancient_Kraken_V1.png/revision/latest/scale-to-width-down/185?cb=20250220090124",
+    "https://static.wikia.nocookie.net/fisch/images/d/d1/Aurora_Northstar_Serpent.png/revision/latest?cb=20241215091636",
+]
+const imageElement = document.getElementById("GalleryImage");
+const prevButton = document.getElementById("PrevBut");
+const nextButton = document.getElementById("NextBut")
+let currentImgIndex = 0
